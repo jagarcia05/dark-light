6aa2b1cd4bd8e4e34e631a7198667cee57fc4e95
jagarcia05 <joseantoniogarciapascual17@gmail.com>
2025-02-25 08:37:30 +0100

    pepe

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  base: "/", // Asegura que la base es correcta
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  preview: {
+    port: 5000,
+    open: true,
+  }
 });
